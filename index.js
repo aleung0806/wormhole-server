381e807aea3e7849cb2d7971a165d3930d05bf27
aleung0806 <aleung0806@gmail.com>
2023-09-28 20:36:33 -0700

    added toggle env

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const app = require("./app");
 const http = require("http");
 
-const dotenv = require('dotenv');
-const path = require('path');
 
-dotenv.config({ path: path.join(__dirname, './.env') });
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, `./.env.${process.env.NODE_ENV}`) });
 
 const server = http.createServer(app);
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 
 server.listen(port, () => {
-  console.log(`server running on port ${port}`);
+  console.log(`${process.env.NODE_ENV} mode. listening on port ${port}.`);
 });
