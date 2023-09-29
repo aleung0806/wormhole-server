dc81db357727d9a2487b4f081930f7e24a364455
aleung0806 <aleung0806@gmail.com>
2023-09-28 20:24:49 -0700

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require("./app");
 const http = require("http");
 
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.join(__dirname, './.env') });
+
 const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
